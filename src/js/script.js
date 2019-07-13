59d4cb4b1bc0efe798f89c164be3085770747cf5
Jose Antonio Valga Casachahua <jvalga@lan.wundermanphantasia.pe>
2019-07-12 20:03:42 -0500

    cyber

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,21 @@ var app = (function () {
 
 	var input = document.getElementById('input-task'),
 		button = document.getElementById('add'),
-		checks = document.querySelectorAll('.check');
+		checks = document.querySelectorAll('.check'),
+		list = document.getElementById('listTask')
+
+	// Private Methods
+	var createElement = function () {
+		var ele = document.createElement('li');
+		var li = '<div class="check">' + '</div>' + '<p>' + input.value + '</p>' + '<div class="delete"></div>';
+		ele.insertAdjacentHTML('afterbegin', li)
+		return ele
+	}
 
 	var addTask = function () {
 		button.addEventListener('click', function () {
-			console.log(input.value)
+			console.log(input.value, this.id);
+			list.appendChild(createElement());
 			input.value = '';
 			input.focus();
 		})
@@ -14,6 +24,7 @@ var app = (function () {
 
 	var checkTask = function () {
 		for (let check of checks) {
+			console.log(check)
 			check.addEventListener('click', function () {
 				if (this.classList.contains('active')) {
 					this.classList.remove('active');
@@ -24,11 +35,14 @@ var app = (function () {
 		}
 	}
 
+
 	return {
 		addTask: addTask,
-		checkTask: checkTask
+		checkTask: checkTask,
+		createElement: createElement
 	}
 })();
 
 app.addTask();
+app.createElement();
 app.checkTask();
